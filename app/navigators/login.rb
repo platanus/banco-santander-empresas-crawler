4f6df752bb4a6ed125a7eb3f5769aba0d8b38bc1
Amós Rivera <arivera.utpmp@gmail.com>
2016-04-01 16:21:33 -0300

    refactor(): remove unnecesary file and code

--- app/navigators/login.rb
+++ app/navigators/login.rb
@@ -6,7 +6,6 @@ class Login < Crabfarm::BaseNavigator
     browser.search("input#d_rut").set(params[:rut])
     browser.search("input#d_pin").set(params[:password])
     browser.search("#botonenvio").click()
-    reduce_with_defaults
   end
 
 end
--- app/reducers/login_reducer.rb
+++
@@ -1,8 +0,0 @@
-class LoginReducer < Crabfarm::BaseReducer
-
-  def run
-    # Put some data extraction code here.
-  end
-
-end
-
