e37986351dd1ab9b9b6afda0edd6917d6560c631
Agustin Feuerhake <agustin@platan.us>
2016-04-01 17:52:16 -0300

    fix(): can't modify an ENV variable

--- spec/navigators/deposit_spec.rb
+++ spec/navigators/deposit_spec.rb
@@ -10,6 +10,6 @@ describe Deposit do
   it "should reach the post deposit page with success" do
     navigate amount: ENV['SPEC_AMOUNT'], destinatary_account: ENV['SPEC_ACCOUNT'], destinatary_bank: ENV['SPEC_BANK'], destinatary_rut: ENV['SPEC_RUT'],destinatary_name: ENV['SPEC_NAME']
     browser.search("a:contains('Transferencias Creadas')").click
-    expect(browser.search("#Tcreadas").text).to include(ENV['SPEC_ACCOUNT'].insert(-2, "-"))
+    expect(browser.search("#Tcreadas").text).to include(ENV['SPEC_ACCOUNT'].dup.insert(-2, "-"))
   end
 end
