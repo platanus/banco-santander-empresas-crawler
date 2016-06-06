f7f852abb83de9bac30918d805ad4a53493113c6
Nicolas Mery <njmery@gmail.com>
2016-06-06 18:40:11 -0400

    chore(): added deposit deletion

---
+++ spec/navigators/deposit_deletion_spec.rb
@@ -0,0 +1,17 @@
+require 'spec_helper'
+
+describe DepositDeletion do
+
+  it "should reach the post deposit deletion page with success" do
+    navigate amount: ENV['SPEC_AMOUNT'],
+             destinatary_account: ENV['SPEC_ACCOUNT'],
+             destinatary_bank: ENV['SPEC_BANK'],
+             destinatary_rut: ENV['SPEC_RUT'],
+             destinatary_name: ENV['SPEC_NAME'],
+             rut: ENV['RUT'],
+             password: ENV['PASSWORD'],
+             from_account: ENV['SPEC_FROM_ACCOUNT'],
+             coords: ENV['SPEC_COORDS']
+    expect(browser.search("#estatusproceso").wait(:present).text).to include("Fueron eliminados satisfactoriamente los siguientes registros")
+  end
+end
