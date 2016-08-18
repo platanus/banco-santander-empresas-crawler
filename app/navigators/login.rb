880f84a3ae609ac38f72b4ff31715bfeee38d29e
Nicolas Mery <njmery@gmail.com>
2016-08-18 11:29:08 -0300

    chore(): handle case when there is an unkown authentication error message

--- app/navigators/login.rb
+++ app/navigators/login.rb
@@ -6,7 +6,10 @@ class Login < Crabfarm::BaseNavigator
     browser.search("input#d_rut").set(params[:rut])
     browser.search("input#d_pin").set(params[:password])
     browser.search("#botonenvio").click()
-    if params[:from_account]
+    raise "error de autenticacion" if browser.search("font:contains('Error de autenticación desconocido')").present?
+
+    if !params[:from_account].blank?
+      puts "FROM ACCOUNT #{params[:from_account]}"
       browser.goto frame:"[name='derecho']"
       browser.search("#tablalista > table > tbody > tr").each do |trx|
         if trx.search("td")[1].text.to_i == params[:from_account].to_i &&
